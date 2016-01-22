eb5905d74a425135f4ead534faf852bac4dd6a39
mesb <eng.mesembe@gmail.com>
2016-01-22 00:17:53 -0500

    Adds gates and tests for the gates This commit adds a couple of logic gate functions and their tests

--- gates.go
+++ gates.go
@@ -1,12 +1,62 @@
+// This package implements basic logic gates.
+// It uses Nand as the most basic gate and build all others from it
+
 package gates
 
-// This package implements basic logic gates
+
+import (
+	_"math"
+	_"fmt"
+)
 
 // Bit
 type Bit uint8
 
+// Unary function type
+type UnaryFunc func(Bit) Bit
+
+// Binary function type
+type BinaryFunc func(Bit, Bit) Bit
+
+// n-bit function type
+type MultiBitBinFunc func([]Bit, []Bit) []Bit
+
+// n-bit unary function type
+type MultiBitUnaryFunc func([]Bit) []Bit
+
+// Construct an n-Bit Binary function
+func NewMultiBitBinFunc(nbits int, funcName BinaryFunc) MultiBitBinFunc {
+
+	out := make([]Bit, nbits)
+	f := func(a, b []Bit) []Bit {
+		for i:= 0; i < nbits-1; i++ {
+			out[i] = funcName(a[i], b[i])
+		}
+
+		return out
+	}
+
+	return f
+}
+
+
+// Constructor for n-bit Unary function
+func NewMultiBitUnaryFunc(nbits int, funcName UnaryFunc) MultiBitUnaryFunc {
+	out := make([]Bit, nbits)
+	f := func(a []Bit) []Bit {
+		for i := 0; i < nbits-1; i++ {
+			out[i] = funcName(a[i])
+		}
+
+		return out
+
+	}
+
+	return f
+}
 
 // Nand gate. This is the building block for all other gates
+// if a=b=1 then out=0 else out=1
 func Nand(a, b Bit) Bit {
 	if a == b && a == 1 {
 		return 0
@@ -16,6 +66,95 @@ func Nand(a, b Bit) Bit {
 }
 
 // Not gate
-func Not(a Bit) Bit {
-	return Nand(a, a)
+// If in=0 then out=1 else out=0
+func Not(in Bit) Bit {
+	return Nand(in, in)
+}
+
+// And receives 2 input: a, b and returns out
+// if a=b=1 then out=1 else out=0
+func And(a, b Bit) Bit {
+	in := Nand(a, b)
+
+	return Not(in)
+}
+
+
+// Or
+// input: a, b
+// output: out
+// if a=b=0 then out=0 else out=1
+func Or(a, b Bit) Bit{
+	a = Not(a)
+	b = Not(b)
+	return Nand(a, b)
+}
+
+
+// Xor
+// input: a, b
+//output: out
+// if a!=b then out=1 else out= 0
+
+func Xor(a, b Bit) Bit{
+	i := Nand(a, Not(b))
+	j := Nand(Not(a), b)
+	return Nand(i, j)
+}
+
+
+// Mux: Multiplexor
+// inputs a, b, sel
+//outputs: out
+// if sel=0 then out=a else out=b
+func Mux(a, b, sel Bit) Bit{
+	a = And(a, Not(sel))
+	b = And(b, sel)
+	return Or(a, b)
+}
+
+
+// DMux: Demultuplexor
+// inputs in, sel
+// outputs: a, b
+// Functions: If sel=0 then (a=in, b=0) else (a=0, b=in)
+func DMux(in, sel Bit) (a, b Bit) {
+	a = And(in, Not(sel))
+	b = And(in, sel)
+
+	return a, b
+}
+
+
+// Not16
+// Inputs: in[16] // 1 16-bit pin
+// out[16]
+// for i=0..15 out[i] = Not(in[i])
+var Not16 = NewMultiBitUnaryFunc(16, Not)
+
+
+var And16 = NewMultiBitBinFunc(16, And)
+
+
+func pow(a, b int) int{
+	if b == 0 {
+		return 1
+	}
+
+	return a * pow(a, b-1)
+}
+
+
+// Convert a binary number to a decimal
+func BinToDec(b []Bit) int{
+	sum := 0
+	n := len(b)
+
+	for i := 0; i < n; i++  {
+		p := pow(2, n-1-i)
+
+		sum += (p * int(b[i]))
+	}
+
+	return sum
 }
--- gates_test.go
+++ gates_test.go
@@ -1,5 +1,5 @@
+// Tests for the gates package
 package gates
-// Testing the gates package
 
 import (
 	"testing"
@@ -46,4 +46,184 @@ func TestNot(t *testing.T) {
 		}
 	}
 
+}
+
+// Test for the And gate
+func TestAnd(t *testing.T) {
+		cases :=  []struct {
+	a, b, out Bit
+	}{
+		{0, 0, 0},
+		{0, 1, 0},
+		{1, 0, 0},
+		{1, 1, 1},
+
+	}
+
+	for _, c := range cases {
+		got := And(c.a, c.b)
+		if got != c.out {
+			t.Errorf("And(%v, %v) = %v", c.a, c.b, c.out)
+		}
+	}
+
+}
+
+
+
+
+// Test for the Or gate
+func TestOr(t *testing.T) {
+		cases :=  []struct {
+	a, b, out Bit
+	}{
+		{0, 0, 0},
+		{0, 1, 1},
+		{1, 0, 1},
+		{1, 1, 1},
+
+	}
+
+	for _, c := range cases {
+		got := Or(c.a, c.b)
+		if got != c.out {
+			t.Errorf("Or(%v, %v) = %v. Expected %v", c.a, c.b, got, c.out)
+		}
+	}
+
+}
+
+
+
+// Test for the Xor gate
+func TestXor(t *testing.T) {
+		cases :=  []struct {
+	a, b, out Bit
+	}{
+		{0, 0, 0},
+		{0, 1, 1},
+		{1, 0, 1},
+		{1, 1, 0},
+
+	}
+
+	for _, c := range cases {
+		got := Xor(c.a, c.b)
+		if got != c.out {
+			t.Errorf("Or(%v, %v) = %v. Expected %v", c.a, c.b, got, c.out)
+		}
+	}
+
+}
+
+
+// Test Mux
+func TestMux(t *testing.T) {
+		cases :=  []struct {
+			a, b, sel, out Bit
+	}{
+		{0, 0, 0, 0},
+		{0, 1, 0, 0},
+		{1, 0, 0, 1},
+		{1, 1, 0, 1},
+		{0, 0, 1, 0},
+		{0, 1, 1, 1},
+		{1, 0, 1, 0},
+		{1, 1, 1, 1},
+
+	}
+
+	for _, c := range cases {
+		got := Mux(c.a, c.b, c.sel)
+		if got != c.out {
+			t.Errorf("Mux(%v, %v, %v) = %v. Expected %v", c.a, c.b, c.sel, got, c.out)
+		}
+	}
+
+}
+
+
+
+
+// Test DMux
+func TestDMux(t *testing.T) {
+		cases :=  []struct {
+			in, sel, a, b Bit
+	}{
+		{0, 0, 0, 0},
+		{0, 1, 0, 0},
+		{1, 0, 1, 0},
+		{1, 1, 0, 1},
+		//{0, 0, 0, 0},
+		//{0, 1, 0, 0},
+		//{1, 0, 1, 0},
+		//{1, 1, 0, 1},
+
+	}
+
+	for _, c := range cases {
+		a, b := DMux(c.in, c.sel)
+		if a != c.a && b != c.b {
+			t.Errorf("DMux(%v, %v) = %v %v. Expected %v %v", c.in, c.sel, a,
+		b, c.a , c.b)
+		}
+	}
+
+}
+
+// Test for Not16
+func TestNot16(t *testing.T) {
+	cases := struct {
+		in, out []Bit
+	}{
+		[]Bit{1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+		[]Bit{0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	}
+
+	got := Not16(cases.in)
+	for i, e := range got {
+		if got[i] != cases.out[i] {
+			t.Errorf("Error computing bit number %v: Not(%v) = %v, expected %v", i,
+				e, e, cases.out[i] )
+		}
+	}
+}
+
+
+// Test the power function
+func TestPow(t *testing.T) {
+	cases := []struct{
+		b, e,  out int
+	}{
+		{2, 0, 1},
+		{3, 0, 1},
+		{2, 3, 8},
+		{3, 3, 27},
+	}
+
+	for _, c := range cases {
+		got := pow(c.b, c.e)
+		if got != c.out {
+			t.Errorf("pow(%v, %v) = %v. Expected %v", c.b, c.e, got, c.out)
+		}
+	}
+
+}
+
+
+func TestBinToDec(t *testing.T) {
+	cases := []struct{
+		n []Bit
+		out int
+	}{
+		{[]Bit{1, 0, 0, 1, 1}, 19},
+		{[]Bit{0, 0, 0, 0, 0}, 0},
+	}
+
+	for _, c := range cases {
+		got := BinToDec(c.n)
+		if got != c.out {
+			t.Errorf("BinToDec(%v) = %v. Expected %v\n", c.n, got, c.out)
+		}
+	}
 }
